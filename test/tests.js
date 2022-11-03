da52ccbc2f1cf24d1893387a6e5788a75c9213ff
ANDREW VALENZUELA <ANDREWJV93@GMAIL.COM>
2022-11-03 11:59:24 -0700

    completed the first two tests

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,5 @@
 // IMPORT MODULES under test here:
-import { myFunction } from '../functions.js';
+import { addExclamationPoints, multiplyBySeven, myFunction } from '../functions.js';
 
 const { test, skip } = QUnit;
 
@@ -17,4 +17,32 @@ skip('this test should be skipped', (expect) => {
     const actual = true;
 
     expect.equal(actual, expected);
+});
+
+test('this test should add 3 exclamation points', (expect) => {
+    const expected1 = 'bunny rabbit!!!';
+    const actual1 = addExclamationPoints('bunny rabbit');
+    expect.equal(actual1, expected1);
+
+    const expected2 = 'dog!!!';
+    const actual2 = addExclamationPoints('dog');
+    expect.equal(actual2, expected2);
+
+    const expected3 = 'horse!!!';
+    const actual3 = addExclamationPoints('horse');
+    expect.equal(actual3, expected3);
+});
+
+test('this test should equal 28', (expect) => {
+    const expected1 = '28';
+    const actual1 = multiplyBySeven('4');
+    expect.equal(actual1, expected1);
+
+    const expected2 = '49';
+    const actual2 = multiplyBySeven('7');
+    expect.equal(actual2, expected2);
+
+    const expected3 = '21';
+    const actual3 = multiplyBySeven('3');
+    expect.equal(actual3, expected3);
 });
